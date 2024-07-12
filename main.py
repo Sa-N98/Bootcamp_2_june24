384bc94e1cf313ac350a5fdbeceabc5dae6d2889
Saranya Nayak <135203195+Sa-N98@users.noreply.github.com>
2024-07-12 21:27:37 +0530

    day 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 from model import * 
 
@@ -13,9 +13,85 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(current_dir,
 db.init_app(app)
 app.app_context().push() # program stack
 
-@app.route('/', methods=["GET"])
-def home():
-    return render_template('home.html')
+# @app.route('/', methods=["GET"]) 
+# #  / = http://192.168.43.103:5000/
+# def home():
+#     return render_template('home.html')
+
+
+
+
+@app.route('/signup', methods=['GET' , 'POST'])
+def signup():
+   
+    if request.method == 'POST':
+        username_value = request.form['username'] 
+        email_value = request.form['email'] 
+        password_value = request.form['password']
+        user_type_value = request.form['user_type']  
+
+        
+        
+        user_data= users.query.filter_by(email = email_value).first()
+
+        if user_data == None:
+            newUser = users(name = username_value,
+                             email =email_value , 
+                             password =password_value , 
+                             user_type=user_type_value)
+            db.session.add(newUser)
+            db.session.commit()
+            return render_template('user_added.html')
+        return render_template( 'userr_notadded.html' , error_message= ' Registration Successful!', redirect_massage = 'Click here to login' )
+
+
+       
+        # if user_data != None:
+        #     print()
+        #     print("name ==>" ,user_data.name)
+        #     print("email ==>" ,user_data.email)
+        #     print("user type ==>" ,user_data.user_type)
+        #     print()
+        # else:
+        #     print()
+        #     print( 'user does not exists')
+        #     print()
+
+
+    return render_template('signup.html')
+
+
+
+@app.route('/', methods=['GET' , 'POST'])
+def login():
+
+    if request.method == 'POST':
+        
+        email_value = request.form['email'] 
+        password_value = request.form['password']
+
+        user_data= users.query.filter_by(email = email_value).first()
+
+        if user_data != None:
+            if user_data.password == password_value:
+
+                if user_data.user_type == 'commoner':
+                    return "user dashbord template"
+                elif user_data.user_type == 'artist':
+                    return "artist dashbord template"
+                
+        return render_template( 'userr_notadded.html', error_message= "User dosen't exists ", redirect_massage = 'Click here to Signup')
+
+
+
+
+
+
+    return render_template('login.html')
+    
+    
+    
+
 
 if __name__ == "__main__" :
     db.create_all()
